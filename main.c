0a8fc53114a69feddf528d842a20d161eedd5c91
Remsko <pinoit.remi@gmail.com>
2020-02-28 16:57:21 +0100

    strlen

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
-int main(void)
-{
+#include <stdio.h>
+
+int ft_strlen(const char *s);
+
+int main() {
+    printf("length: %d ; expect 4\n", ft_strlen("test"));
+    printf("length: %d ;  expect 0\n", ft_strlen(""));
+    printf("length: %d ; expect 11\n", ft_strlen("hello world"));
+    printf("length: %d ; expect 3\n", ft_strlen("bla"));
+    //printf("length: %d ; expect 0\n", ft_strlen(NULL));
+
     return 0;
 }
---
+++ srcs/ft_strlen.s
@@ -0,0 +1,14 @@
+global _ft_strlen
+
+section .text
+
+_ft_strlen:
+        sub     rcx, rcx    ; rcx = 0
+        not     rcx         ; rcx = -1
+        sub     al, al      ; al = 0
+        cld                 ; clear df flag
+repne   scasb               ; search al in rdi
+        not     rcx         ; ecx = - strlen - 2 and |ecx| = ecx - 1 so |ecx| - 1 = strlen 
+        ;dec     rcx         ; ecx = strlen
+        lea     rax, [rcx - 1] ; 
+        ret
